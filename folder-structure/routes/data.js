883d87a10343ef170c3182a569671a5b497fea20
junaisbabu <junaisdvs@gmail.com>
2023-03-13 01:53:04 +0530

    getAllUsers and getSingleUser data implement using ORM

--- folder-structure/routes/data.js
+++ folder-structure/routes/data.js
@@ -1,34 +1,38 @@
-//! this file needs `app` variable
-
-//! const app = express(); --> (not doing this again) It will be an full app
-
 const express = require("express");
-require('../models/user_new');
-
-// const { getUserDataByName, getAllUser } = require("../models/user_old");
-// require("../models/connection");
+const { User } = require("../models/user_new");
 
 const route = express.Router();
 
-route.get("/", function (req, res) {
+route.get("/", async function (req, res) {
   let sortBy = req.query.sortBy || "";
 
-  getAllUser({ sortBy }).then((result) => {
-    res.render("tableData", { data: result.rows });
-  });
+  // getAllUser({ sortBy }).then((result) => {
+  //   res.render("tableData", { data: result.rows });
+  // });
+
+  const results = await User.findAll({ orderBy: sortBy }); //! I don't create `findAll`, findAll given to me from sequelize
+
+  res.render("tableData", { data: results.map((el) => el.dataValues) });
 });
 
 route.get("/:user", async function (req, res) {
   const username = req.params.user;
 
-  //! Promise-style code
-  //   getUserDataByName(username).then((result) => {
-  //     res.render('singleUser', {name: result.rows[0].name, age: result.rows[0].age})
-  //   });
+  // const data = await getUserDataByName(username);
+  // res.render("singleUser", { name: data.rows[0].name, age: data.rows[0].age });
 
-  //! async style code
-  const data = await getUserDataByName(username);
-  res.render("singleUser", { name: data.rows[0].name, age: data.rows[0].age });
+  const data = await User.findOne({
+    where: {
+      name: username,
+    },
+  });
+
+  console.log(data);
+
+  res.render("singleUser", {
+    name: data.dataValues.name,
+    age: data.dataValues.age,
+  });
 });
 
 module.exports = {
